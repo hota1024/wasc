b0a922e8210ca2327233b93e0142b062e69dc922
= <hotalog@hota1024.com>
2022-11-24 22:36:58 +0900

    🔥 clean project

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,6 @@
-use compiler::Compiler;
-use lexer::{lex, remove_whitespace_tokens};
-use parsers::parse;
-use sexpr::Expr;
-use token_walker::TokenWalker;
 use wasm_bindgen::prelude::*;
-
-mod ast;
-mod ast_to_sexpr;
-mod compiler;
-mod lexer;
-mod parse_err;
-mod parse_utils;
-mod parser_common;
-mod parsers;
-mod sexpr;
-mod span;
-mod token_walker;
-mod tokens;
-
-#[wasm_bindgen]
-pub fn compile_result(input: &str) -> Result<String, String> {
-    if input.len() == 0 {
-        Err("input is required".to_string())
-    } else {
-        Ok("ok".to_string())
-    }
-}
 
 #[wasm_bindgen]
 pub fn compile(input: &str) -> String {
-    let tokens = remove_whitespace_tokens(lex(input));
-    let mut walker = TokenWalker::new(tokens);
-    let ast = parse(&mut walker).unwrap();
-    let expr = ast_to_sexpr::ast_to_sexpr(&ast).unwrap();
-    //let mut compiler = Compiler::new();
-    //let expr = compiler.compile(ast).unwrap();
-    //panic!("test");
-
-    return Expr::encode(&expr);
+    input.to_string()
 }
